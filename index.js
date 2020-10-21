84b0530432885a2570d94095d3f8f844bbc1a208
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-21 16:57:27 -0700

    Add content management tokens

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: "lhmy5mgb9x5k",
+  accessToken: "pD7aIO7WDf28G0qD_jScmSX_vvgNOuLk3DOd95P5HpI",
+});
+
+client.getEntries((entries) => {
+  entries.forEach((entry) => console.log(entry));
+});
