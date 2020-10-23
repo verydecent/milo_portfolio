2211b0dfaf11be8d00353d483bdab898e9e48c69
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-22 21:04:26 -0700

    update mobile nav

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
 // Mobile Nav
-const mobileNav = document.querySelector(".mobile-nav");
-const navColumn = document.querySelector(".nav-column");
+const mobileNavBurger = document.querySelector(".mobile-nav-burger");
+const mobileNavColumn = document.querySelector(".mobile-nav-column");
+const mobileNavExitButton = document.querySelector(".mobile-nav-exit-button");
 
-mobileNav.addEventListener("click", function () {
-  if (navColumn.style.display === "none") {
-    navColumn.style.display = "block";
+mobileNavBurger.addEventListener("click", function () {
+  if (mobileNavColumn.style.display === "block") {
+    mobileNavColumn.style.display = "none";
   } else {
-    navColumn.style.display = "none";
+    mobileNavColumn.style.display = "block";
   }
 });
-console.log(mobileNav);
 
+// Mobile Nav Exit Button
+
+mobileNavExitButton.addEventListener("click", function () {
+  mobileNavColumn.style.display = "none";
+});
+
+// Mobile Sliding
+const mobileShowsListButton = document.getElementById(
+  "mobile-shows-list-button"
+);
+const mobileShowsList = document.querySelector(".mobile-shows-list");
+
+mobileShowsListButton.addEventListener("click", function () {
+  if (mobileShowsList.className === "shows-list") {
+    mobileShowsList.className = "shows-list show-list-appear";
+  } else {
+    mobileShowsList.className = "shows-list";
+  }
+});
 // Nav Sliding
 const showsListButton = document.getElementById("shows-list-button");
 const showsList = document.querySelector(".shows-list");
